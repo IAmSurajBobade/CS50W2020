600c0ef68e5a7534e479af57ce1d6d8670d271a7
Suraj Bobade <iamsurajbobade@gmail.com>
2020-08-22 18:50:41 +0530

    Lecture 4: SQL

---
+++ lect4_SQL/airline/users/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+
+# Create your views here.
+
+
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    return render(request, "users/user.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = password = ''
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/login.html", {
+                "message": "Invalid credentials."
+            })
+    return render(request, "users/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return render(render, "user/login.html", {
+        "message": "Logged out."
+    })
